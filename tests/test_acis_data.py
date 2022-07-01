2161fdc5f66b91cdc2fed5797cac82bd548af55b
Alex Young <45477688+alex-l-young@users.noreply.github.com>
2022-07-01 15:06:57 -0400

    Create test_acis_data.py

---
+++ tests/test_acis_data.py
@@ -0,0 +1,39 @@
+# ====================================================================================================
+# Test script for pydrology/Data_Requests/usgs_data.py
+# ====================================================================================================
+
+# Library imports.
+import pandas as pd
+
+# Local imports.
+from pydrology.Data_Requests.acis_data import request_acis_data
+
+class TestClassAcis:
+    def setup(self):
+        self.site_id = '304174 2'  # Ithaca.
+        self.met_elements = ['maxt', 'mint', 'snow', 'avgt', 'pcpn']
+        self.start_date = '2022-01-01'  # YYYY-mm-dd.
+        self.end_date = '2022-01-21'
+
+    def test_request_acis_data(self):
+        acis_df = request_acis_data(
+            self.met_elements,
+            self.site_id,
+            self.start_date,
+            self.end_date
+        )
+
+        # List of assertion errors.
+        errors = []
+        # Test if the gage_df is a Pandas DataFrame.
+        if not type(acis_df) == pd.DataFrame:
+            errors.append('Gage DataFrame not generated.')
+        # Test if data frame contains data.
+        if acis_df.empty:
+            errors.append("Gage DataFrame is empty.")
+        # Test if the correct columns are present.
+        cols = ['Date', 'MaxTemp', 'MinTemp', 'SnowDepth', 'AvgTemp', 'Precipitation', 'site_id', 'name', 'latitude', 'longitude']
+        if set(cols) != set(acis_df.columns):
+            errors.append("Not all ACIS columns are present.")
+
+        assert not errors, "Errors occured:\n{}".format("\n".join(errors))
