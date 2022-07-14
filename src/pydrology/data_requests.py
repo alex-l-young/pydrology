c13ee718ee5a60351bcef1643ced3b4520222b3a
Alex Young <45477688+alex-l-young@users.noreply.github.com>
2022-07-14 14:12:57 -0400

    Add option for height or discharge parameter output

--- src/pydrology/data_requests.py
+++ src/pydrology/data_requests.py
@@ -112,7 +112,7 @@ def acis_to_csv(acis_df, csv_dir):
 # =======================================================
 # USGS Stream Gage Data.
 # =======================================================
-def request_usgs_data(gage_id, start_date, start_time, end_date, end_time, gmt_offset):
+def request_usgs_data(gage_id, parameter, start_date, start_time, end_date, end_time, gmt_offset):
     """
 
     :param gage_id [string]: ID of gage to request data for.
@@ -121,25 +121,31 @@ def request_usgs_data(gage_id, start_date, start_time, end_date, end_time, gmt_o
     :param end_date [string]: End date in format yyyy-mm-dd.
     :param end_time [string]: Local end time in format HH:MM:SS.mmm
     :param gmt_offset [string]: Number of hour offset from gmt (+ or -) in format +/-HH:MM. "-04:00"
+    :param parameter [string]: 'discharge' or 'height'
     :return:
     """
+    # Convert parameter name to the code.
+    if parameter == 'discharge':
+        code = '00060'
+    else:
+        code = '00065'
 
     # Populate the request URL with ID and dates.
-    usgs_url_format = ("https://waterservices.usgs.gov/nwis/iv/?sites={}&parameterCd=00065&"
+    usgs_url_format = ("https://waterservices.usgs.gov/nwis/iv/?sites={}&parameterCd={}&"
                        "startDT={}T{}{}&endDT={}T{}{}&"
                        "siteStatus=all&format=rdb")
-    usgs_url = usgs_url_format.format(gage_id, start_date, start_time, gmt_offset, end_date, end_time, gmt_offset)
+    usgs_url = usgs_url_format.format(gage_id, code, start_date, start_time, gmt_offset, end_date, end_time, gmt_offset)
 
     r = requests.get(usgs_url)
     r_text = r.text
 
-    gage_df = create_discharge_df(r_text)
+    gage_df = create_discharge_df(r_text, parameter)
 
     return gage_df
 
 
-def create_discharge_df(usgs_text):
-    cols = ['agency', 'site_no', 'datetime', 'tz_cd', 'discharge', 'provis_accept']
+def create_discharge_df(usgs_text, parameter):
+    cols = ['agency', 'site_no', 'datetime', 'tz_cd', parameter, 'provis_accept']
     skiprows = 25
 
     # Keep incrementing the number of rows to skip until the correct number is reached.
